f8b0252874e693b73d6dcd13de2c7f1b8b614197
Rohit Kumar <rohit9075@gmail.com>
2018-11-06 20:09:45 +0530

    Scale animation

--- app/src/main/java/com/rohit/com/tweenanimation/MainActivity.java
+++ app/src/main/java/com/rohit/com/tweenanimation/MainActivity.java
@@ -3,18 +3,21 @@ package com.rohit.com.tweenanimation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    Animation mAlphaAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.imageView_ball);
+      imageView =   findViewById(R.id.imageView_ball);
     }
 
     public void rotateAnimation(View view) {
@@ -27,5 +30,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void alphaAnimation(View view) {
+
+        mAlphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_anim);
+        imageView.startAnimation(mAlphaAnimation);
     }
 }
